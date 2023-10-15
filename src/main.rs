2825883e5409936e27b0b9471cae9a98f43010b5
aaronknudtson <aaron.knudtson@outlook.com>
2023-10-15 09:11:13 -0400

    bitwise math for the win

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![no_std]
 #![no_main]
 
+use embedded_hal::digital::v2::{PinState, OutputPin};
 use panic_halt as _;
-use arduino_hal::{port::{mode::Output, Pin}, hal::port::{PB5, PB4, PB2, PB1, PB0, PD4, PD5, PD7}};
+use arduino_hal::{port::{mode::Output, Pin}, hal::port::Dynamic};
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -26,139 +27,49 @@ fn main() -> ! {
 }
 
 struct Segments {
-    e: Pin<Output, PB5>,
-    d: Pin<Output, PB4>,
-    c: Pin<Output, PB2>,
-    dp: Pin<Output, PB1>,
-    b: Pin<Output, PB0>,
-    a: Pin<Output, PD7>,
-    f: Pin<Output, PD5>,
-    g: Pin<Output, PD4>,
+    pins: [Pin<Output, Dynamic>; 7],
 }
 
 impl Segments {
     fn new() -> Segments {
         let dp = arduino_hal::Peripherals::take().unwrap();
         let pins = arduino_hal::pins!(dp);
+        let pin_arr: [Pin<Output, Dynamic>; 7] = [
+            pins.d4.into_output().downgrade(),// g
+            pins.d5.into_output().downgrade(), // f
+            pins.d13.into_output().downgrade(), // e
+            pins.d12.into_output().downgrade(), // d
+            pins.d10.into_output().downgrade(), // c
+            pins.d8.into_output().downgrade(), // b
+            pins.d7.into_output().downgrade(), // a
+        ];
 
         Segments {
-            e: pins.d13.into_output(),
-            d: pins.d12.into_output(),
-            c: pins.d10.into_output(),
-            dp: pins.d9.into_output(),
-            b: pins.d8.into_output(),
-            a: pins.d7.into_output(),
-            f: pins.d5.into_output(),
-            g: pins.d4.into_output(),
+            pins: pin_arr
         }
     }
 
     fn display(&mut self, i: u32) {
-        self.clear_display();
-        match i {
-            0 => {
-                self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                self.d.set_high();
-                self.e.set_high();
-                self.f.set_high();
-                //self.g.set_high();
-            }
-            1 => {
-                //self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                //self.d.set_high();
-                //self.e.set_high();
-                //self.f.set_high();
-                //self.g.set_high();
-            }
-            2 => {
-                self.a.set_high();
-                self.b.set_high();
-                //self.c.set_high();
-                self.d.set_high();
-                self.e.set_high();
-                //self.f.set_high();
-                self.g.set_high();
-            }
-            3 => {
-                self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                self.d.set_high();
-                //self.e.set_high();
-                //self.f.set_high();
-                self.g.set_high();
-            }
-            4 => {
-                //self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                //self.d.set_high();
-                //self.e.set_high();
-                self.f.set_high();
-                self.g.set_high();
-            }
-            5 => {
-                self.a.set_high();
-                //self.b.set_high();
-                self.c.set_high();
-                self.d.set_high();
-                //self.e.set_high();
-                self.f.set_high();
-                self.g.set_high();
-            }
-            6 => {
-                self.a.set_high();
-                //self.b.set_high();
-                self.c.set_high();
-                self.d.set_high();
-                self.e.set_high();
-                self.f.set_high();
-                self.g.set_high();
-            }
-            7 => {
-                self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                //self.d.set_high();
-                //self.e.set_high();
-                //self.f.set_high();
-                //self.g.set_high();
-            }
-            8 => {
-                self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                self.d.set_high();
-                self.e.set_high();
-                self.f.set_high();
-                self.g.set_high();
-            }
-            9 => {
-                self.a.set_high();
-                self.b.set_high();
-                self.c.set_high();
-                self.d.set_high();
-                //self.e.set_high();
-                self.f.set_high();
-                self.g.set_high();
-            }
-            _ => {}
-        };
-        arduino_hal::delay_ms(1000);
+        let bits = Segments::map_bits(i);
+        for n in 0..7 {
+            let _ = self.pins[n].set_state(PinState::from(bits & 1 << n != 0));
+        }
+        arduino_hal::delay_ms(200);
     }
 
-    fn clear_display(&mut self) {
-        self.e.set_low();
-        self.d.set_low();
-        self.c.set_low();
-        self.dp.set_low();
-        self.b.set_low();
-        self.a.set_low();
-        self.f.set_low();
-        self.g.set_low();
+    fn map_bits(i: u32) -> u8 {
+        return match i {
+            0 => 0b01111110,
+            1 => 0b00110000,
+            2 => 0b01101101,
+            3 => 0b01111001,
+            4 => 0b00110011,
+            5 => 0b01011011,
+            6 => 0b01011111,
+            7 => 0b01110000,
+            8 => 0b01111111,
+            9 => 0b01111011,
+            10.. => 0
+        }
     }
 }
